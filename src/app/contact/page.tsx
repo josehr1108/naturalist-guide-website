b72854c2ff27649fbdf63912ab814b7c91587191
Jose Herrera <josehr1108@gmail.com>
2025-10-27 23:51:47 -0600

    add contact email functionality, no design for sent email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import HeroSection from "../_components/HeroSection/HeroSection";
 import { MapPin, Phone, Mail } from "lucide-react";
+import { sendMail } from "../actions/sendMail";
+import { useActionState, useEffect } from "react";
+import Swal from "sweetalert2";
 
 const HERO_TITLE = "Let's Get In Touch";
 const HERO_IMAGE_SRC = "/images/arenal-contact-bg.png";
@@ -7,7 +12,38 @@ const HERO_DESCRIPTION =
   "Every traveler is unique. Tell me what you’re looking for, and together we’ll create a tailor-made nature experience.";
 const HERO_IMAGE_ALT = "Arenal volcano background";
 
+interface FormState {
+  success: boolean;
+  error?: string;
+}
+
 const Contact = () => {
+  const [state, formAction, isPending] = useActionState(
+    async (state: FormState, formData: FormData) => {
+      return await sendMail(formData);
+    },
+    {
+      success: false,
+      error: "",
+    }
+  );
+
+  useEffect(() => {
+    if (!isPending && state.success) {
+      Swal.fire({
+        icon: "success",
+        title: "Message Sent",
+        text: "Thank you for reaching out! I will get back to you shortly.",
+      });
+    }
+    if (!isPending && state.error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "An error occurred while sending your message. Please try again later.",
+      });
+    }
+  }, [state, isPending]);
   return (
     <main>
       <HeroSection
@@ -66,31 +102,39 @@ const Contact = () => {
                   Your tailor-made adventure starts here.
                 </h3>
 
-                <form className="space-y-4">
+                <form className="space-y-4" action={formAction}>
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <input
+                      name="name"
                       type="text"
                       placeholder="Name *"
+                      required
                       className="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-[var(--color-tasman)]"
                     />
                     <input
+                      name="email"
                       type="email"
                       placeholder="Email *"
+                      required
                       className="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-[var(--color-tasman)]"
                     />
                   </div>
                   <input
+                    name="subject"
                     type="text"
                     placeholder="Subject"
                     className="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-[var(--color-tasman)]"
                   />
                   <textarea
+                    name="message"
+                    required
                     rows={4}
                     placeholder="Comment"
                     className="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-[var(--color-tasman)]"
                   />
                   <button
                     type="submit"
+                    disabled={isPending}
                     className="bg-[var(--color-tasman)] text-primary font-semibold px-6 py-3 rounded-lg shadow transition"
                   >
                     Submit
